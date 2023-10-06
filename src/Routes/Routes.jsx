5dec3647ea30987dd9607befc722d99ac6d0eace
Ador Shahriar <ador.shahriar@gmail.com>
2023-10-06 19:27:46 +0600

    basic installations done with integration of basic routes

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createBrowserRouter } from 'react-router-dom';
+
+const Routes = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello</div>
+    }
+])
+
+export default Routes;
