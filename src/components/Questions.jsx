e929d51b284f4c26e777a991fcf13878c06f2032
Ador Shahriar <ador.shahriar@gmail.com>
2023-10-09 16:18:59 +0600

    updated frequently asked questions section

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -9,7 +9,7 @@ const Questions = () => {
           How can I register for an event through the website?
           </div>
           <div className="collapse-content">
-            <p>Please click the login button on the navbar. If you are not yet a user, then click the register button</p>
+            <p>Please click the login button on the navbar. If you are not yet a user, then click the register button. Then go to the Get Support page and submit the form with required information.</p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-red-200">
@@ -18,16 +18,16 @@ const Questions = () => {
           How do I contact the event organizers if I have questions or issues?
           </div>
           <div className="collapse-content">
-            <p>Get support from our staff form Get Support page after logging in. You can also give us a call on +881234-5678 if you have any questions.</p>
+            <p>Get support from our staff from Get Support page after logging in. You can also give us a call on +881234-5678 if you have any questions.</p>
           </div>
         </div>
         <div className="collapse collapse-arrow join-item border border-red-200">
           <input type="radio" name="my-accordion-4" />
           <div className="collapse-title text-xl font-medium">
-            How much I will be charged if I cancel my event?
+            How much will I be charged if I cancel my event?
           </div>
           <div className="collapse-content">
-            <p>You will be charged a 20% cancellation fee if you cancel before 7 days of the event.</p>
+            <p>You have to inform us before 3 days of the event to get refund. You will be charged a 20% cancellation fee if you cancel before 7 days of the event. If you cancel before 3 days of the event, you will be charged a 40% cancellation fee.</p>
           </div>
         </div>
       </div>
